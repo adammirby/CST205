94b971bb84074894c027f969a2a80eef3678c6fd
Adam Irby <airby@csumb.edu>
2018-04-06 17:22:37 -0700

    added random starting location for player and adjustable player size

--- Player.py
+++ Player.py
@@ -1,27 +1,33 @@
+import random
 #TODO: add inventory local variable
 #TODO: add getItem(item) to add to players inventory
 #TODO: add hasItem() function to report if player has key
 #TODO: add turn variable and modification functions to keep track of number of moves remaining
 
 class Player:
-  location = []
-  lastLocation = []
+  location = []  #keeps track of current location of player in [x, y]
+  lastLocation = [] #keeps track of last location to be used by update to "undraw" last location
+  size = None
   
   #initializes plays location to hard coded value
-  #TODO: change to random starting locaiton
-  #TODO: take map as paramater to see if random starting location is a valid location?
-  def __init__(self):
-    self.location.append(50)
-    self.location.append(50)
-    self.lastLocation.append(50)
-    self.lastLocation.append(50)
+  #DONE: change to random starting locaiton
+  #DONE: take map as paramater to see if random starting location is a valid location?
+  def __init__(self, map, size):
+    locationX = random.randint(0, 499)     #possibly use map functions that return map dimensions?
+    locatitonY = random.randomint(0, 499)  #possibly use map functions that return map dimensions?
+    while map.validLocation([locationX, locationY]) == False:
+      locationX = random.randint(0, 499)
+      locatitonY = random.randomint(0, 499)
+    self.location.append(locationX)
+    self.location.append(locationY)
+    self.size = size
 
   #takes a direction to move the player
   #TODO: determine how far to move player, set distance? let player decide?
   #TODO: needs to check if new location is valid, map class funciton? take map as paramater?
-  def movePlayer(self, direction):
+  def movePlayer(self, map, direction):
     self.lastLocation[0] = self.location[0]
-    self.lastLocation[1] = self.location[1]
+    self.lastLocation[1] = self.location[1] 
     if direction == 'r':
       self.location[0] += 20
     elif direction == 'l':
